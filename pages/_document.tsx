c0a4147acde1aaef2429c619f3fbd4eb23deb095
zhan1999 <zhan1999@yahoo.com>
2023-01-11 22:13:17 +0300

    Add Htag component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+
+  // static async getInitialProps(ctx: DocumentContext):Promise<DocumentInitialProps>{
+  //   const initialProps = await Document.getInitialProps(ctx);
+  //   return( ...initialProps);
+  // }
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
       <body>
         <Main />
         <NextScript />
