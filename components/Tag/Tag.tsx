b4ff88440e9926a4129076a7d4dbef9a60a02c36
zhan1999 <zhan1999@yahoo.com>
2023-01-13 18:17:09 +0300

    Create Tag component

---
+++ components/Tag/Tag.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { TagProps } from './Tag.props';
+import styles from './Tag.module.css';
+import cn from 'classnames';
+
+
+export const Tag = ({ size='s', children, color = 'ghost', href, className, ...props }: TagProps): JSX.Element => {
+	return (
+		<div
+			className={cn(styles.tag, className, {
+				[styles.s]: size == 's',
+				[styles.m]: size == 'm',
+				[styles.ghost]: color == 'ghost',
+				[styles.red]: color == 'red',
+				[styles.green]: color == 'green',
+				[styles.grey]: color == 'grey',
+				[styles.primary]: color == 'primary',
+				}
+			)}
+			{...props}
+		>
+			{
+				href
+					?
+					<a href={href}>{children}</a>
+					:<>{children} </>
+			}
+		</div>
+	);
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 // import { Inter } from '@next/font/google';
 // const inter = Inter({ subsets: ['latin'] });
 
-import { Htag, Button, P } from '../components';
+import { Htag, Button, P, Tag } from '../components';
 
 export default function Home():JSX.Element {
   return (
@@ -12,6 +12,11 @@ export default function Home():JSX.Element {
       <P size='s'>Маленький параграф</P>
       <P>Средний параграф</P>
       <P size='l'>Большой параграф</P>
+      <Tag size='s'>Мал</Tag>
+      <Tag size='m'>Ghost</Tag>
+      <Tag size='s' color='red'>Red</Tag>
+      <Tag size='m' color='green'>Green</Tag>
+      <Tag color='primary'>Primary</Tag>
     </>
 
   );
