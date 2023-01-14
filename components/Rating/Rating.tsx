bdc69e5f5192d787eca116e25cf99efc9facc879
zhan1999 <zhan1999@yahoo.com>
2023-01-14 15:16:44 +0300

    Add mouse and keyboard events to Rating component
    React Hooks: useState and useEffect

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, KeyboardEvent } from 'react';
 import { RatingProps } from './Rating.props';
 import styles from './Rating.module.css';
 import StarIcon from './star.svg';
@@ -6,6 +6,7 @@ import cn from 'classnames';
 
 
 export const Rating = ({ isEditable=false, rating, setRating, ...props }: RatingProps): JSX.Element => {
+	// ratingArray is just a current appearance of rating component, not its value
 	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
 	useEffect(() => {
@@ -13,19 +14,55 @@ export const Rating = ({ isEditable=false, rating, setRating, ...props }: Rating
 	}, [rating]);
 
 	// currentRating = 1..5, i = 0..4
+	// we don't use rating from props because we want to use this function not only 
+	// on props changing but also on hover
 	const constructRating = (currentRating: number) => {
 		const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
 			return (
-				<StarIcon key={i}
-					className={cn(styles.star,
-						{ [styles.filled]: i < currentRating }
-					)}	
+				<span
+				key = {i}
+				className={cn(styles.star, {
+					[styles.filled]: i < currentRating,
+					[styles.editable]: isEditable
+					}
+				)}	
+				onMouseEnter={() => changeDisplay(i + 1)}
+				onMouseLeave={() => changeDisplay(rating)}
+				onClick={() => onClick(i + 1)}
+				>
+
+				<StarIcon
+					tabIndex={isEditable ? 0 : -1}
+					onKeyDown={(e: KeyboardEvent<SVGElement>)=> isEditable && handleSpace(i+1, e) }
 				/>
+				</span>	
 			);
 		});
 		setRatingArray(updatedArray);
 	};
 	
+	const changeDisplay = (i: number) => {
+		if (!isEditable) {
+			return;
+		}
+		constructRating(i);
+	};
+
+	const onClick = (i: number) => {
+		if (!isEditable || !setRating) {
+			return;
+		}
+		setRating(i);
+	};
+	
+	const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+		if (e.code != 'Space' || !setRating) {
+			return;
+		}
+		setRating(i);
+	};
+
+
 	return (
 		<div {...props}>
 			{ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 // import { Inter } from '@next/font/google';
 // const inter = Inter({ subsets: ['latin'] });
 
+import { useState } from 'react';
 import { Htag, Button, P, Tag, Rating } from '../components';
 
 export default function Home():JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+  
   return (
     <>
       <Htag tag='h1'>Текст</Htag>
@@ -17,7 +20,7 @@ export default function Home():JSX.Element {
       <Tag size='s' color='red'>Red</Tag>
       <Tag size='m' color='green'>Green</Tag>
       <Tag color='primary'>Primary</Tag>
-      <Rating rating={4}/>
+      <Rating isEditable rating={rating} setRating={setRating} /> 
     </>
 
   );
