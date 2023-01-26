226956382eae800ccdd4c66e7305b4f9a4559fdc
zhan1999 <zhan1999@yahoo.com>
2023-01-26 19:17:50 +0300

    Create TopPage and TopPageComponent ( was Home)

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -9,18 +9,21 @@ import { ProductModel } from '../../interfaces/product.interface';
 import { ParsedUrlQuery } from 'node:querystring';
 import axios from 'axios';  
 import { firstLevelMenu } from '../../helpers/helpers';
+import { TopPageComponent } from '../../page-components';
 
 // const firstCategory = 0;
 
-function Course({products}: CourseProps):JSX.Element {
+function TopPage({ firstCategory, page, products}: TopPageProps):JSX.Element {
   return (
-    <>
-		{products && products.length} 
-    </>
+		<TopPageComponent 
+			firstCategory = { firstCategory }
+			page = { page }
+			products = {products}
+		/>
   );
 }
 
-export default withLayout(Course);
+export default withLayout(TopPage);
 
 
 
@@ -47,7 +50,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 
 
-export const getStaticProps: GetStaticProps<CourseProps> = async ({params}:GetStaticPropsContext<ParsedUrlQuery>) => {
+export const getStaticProps: GetStaticProps<TopPageProps> = async ({params}:GetStaticPropsContext<ParsedUrlQuery>) => {
 
 	if (!params) {
 		return {
@@ -93,7 +96,7 @@ export const getStaticProps: GetStaticProps<CourseProps> = async ({params}:GetSt
 
 };
 
-interface CourseProps extends Record<string, unknown> {
+interface TopPageProps extends Record<string, unknown> {
 	menu: MenuItem[];
 	firstCategory: TopLevelCategory;
 	page: TopPageModel;
