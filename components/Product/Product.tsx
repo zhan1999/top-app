fe897bcdb6d84041701c3f0e9947d944d88ac7aa
zhan1999 <zhan1999@yahoo.com>
2023-02-17 21:28:10 +0300

    Add aria-hidden attribute & visuallyHidden element

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -65,17 +65,21 @@ export const Product = motion(forwardRef(function ProductFunction({ product, cla
 				</div>
 				<div className={styles.title}>{product.title}</div>
 				<div className={styles.price}>
-					{priceRu(product.price)}
-					{product.oldPrice && <Tag className={styles.oldPrice} color='green'>{priceRu(product.price - product.oldPrice)}</Tag>}
+					<span> <span className='visuallyHidden'>цена</span> {priceRu(product.price)}</span>
+					{product.oldPrice && <Tag className={styles.oldPrice} color='green'><span className='visuallyHidden'>скидка</span>{priceRu(product.price - product.oldPrice)}</Tag>}
 				</div>
 				<div className={styles.credit}>
+					<span className='visuallyHidden'>кредит</span>
 					{priceRu(product.credit)}/
 					<span className={styles.month}>мес</span>
 				</div>
-				<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
+				<div className={styles.rating}>
+					<span className='visuallyHidden'>{'рейтинг' + (Math.round(product.reviewAvg ?? product.initialRating)) }</span>
+					<Rating rating={product.reviewAvg ?? product.initialRating} />
+				</div>
 				<div className={styles.tags}>{product.categories.map(c => <Tag className={styles.category} color='ghost' key={c}>{c}</Tag>)}</div>
-				<div className={styles.priceTitle}>цена</div>
-				<div className={styles.creditTitle}>кредит</div>
+				<div className={styles.priceTitle} aria-hidden={true} >цена</div>
+				<div className={styles.creditTitle} aria-hidden={true}>кредит</div>
 				<div className={styles.rateTitle}><a href="#ref" onClick={scrollToReview}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</a></div>
 				<Divider className={styles.hr} />
 				<div className={styles.description}>{product.description}</div>
